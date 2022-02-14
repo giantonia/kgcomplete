b67d159cf1c5dbacb71978c0b8bdaa4754883ead
giantonia <giang.ngo.nam@gmail.com>
2022-02-14 17:25:26 +0700

    add PathCon

---
+++ PathCon/src/inductive.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+python main.py --inductive --ratio 0.0
+python main.py --inductive --ratio 0.2
+python main.py --inductive --ratio 0.4
+python main.py --inductive --ratio 0.6
+python main.py --inductive --ratio 0.8
+python main.py --inductive --ratio 1.0
